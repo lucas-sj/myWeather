f82011056b47310006b7a622a325d40d31f3b2ac
= <=>
2024-04-17 08:48:49 -0300

    Adding typed location query.

--- scripts.js
+++ scripts.js
@@ -1,35 +1,22 @@
-if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(function(position) {
+// Elements
+const api_key = '3c98783f23b44423d06cd3607435ce4c';
 
-      const lat = position.coords.latitude;
-      const lon = position.coords.longitude;
+const btExternalLocation = document.querySelector("#getExternalLocation")
+const btGetMyLocation = document.querySelector("#getMyLocation");
+const txtExternalCity = document.querySelector("#externalCity");
 
-      callWeatherAPI(lat, lon);
-    }, function(error) {
-      console.error('Erro ao obter a localização:', error.message);
-    });
-  } else {
-    console.error('Geolocalização não suportada pelo navegador.');
-  }
+const locationParagraph = document.getElementById('location');
+const iconWeather = document.getElementById('icon_weather');
+const temp = document.getElementById('temperatura');
+const temp_min = document.getElementById('temperatura_min');
+const temp_max = document.getElementById('temperatura_max');
+const description = document.getElementById('description');
+const humidity = document.getElementById('humidity');
+const speed = document.getElementById('speed');
 
-
-function callWeatherAPI(lat,lon){
-    const lat_test = -23.628300;
-    const lon_test = -46.640900;
-
-    const locationParagraph = document.getElementById('location');
-    const iconWeather = document.getElementById('icon_weather');
-    const temp = document.getElementById('temperatura');
-    const temp_min = document.getElementById('temperatura_min');
-    const temp_max = document.getElementById('temperatura_max');
-    const description = document.getElementById('description');
-    const humidity = document.getElementById('humidity');
-    const speed = document.getElementById('speed');
-
-    const api_key = '3c98783f23b44423d06cd3607435ce4c';
-    const base_url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=pt_br&units=metric&APPID=${api_key}`;
-
-    fetch(base_url)
+//Functions
+function callWeatherAPI(url){
+    fetch(url)
     .then(response => {
       if (!response.ok) {
         throw new Error(`Erro na requisição: ${response.status}`);
@@ -52,9 +39,34 @@ function callWeatherAPI(lat,lon){
     });
 }
 
-
 function getIconAPI(id_icon){
     base_url_icon = `https://openweathermap.org/img/wn/${id_icon}.png`
     return base_url_icon
 }
+
+//Events
+btGetMyLocation.addEventListener("click", (e) => {
+  e.preventDefault();
+  if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+
+      const lat = position.coords.latitude;
+      const lon = position.coords.longitude;
+      const base_url_mylocation = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=pt_br&units=metric&APPID=${api_key}`;
+      callWeatherAPI(base_url_mylocation);
+
+    }, function(error) {
+      console.error('Erro ao obter a localização:', error.message);
+    });
+  } else {
+    console.error('Geolocalização não suportada pelo navegador.');
+  }
+})
+
+btExternalLocation.addEventListener("click", (e) =>{
+  e.preventDefault();
+  const city = txtExternalCity.value;
+  const base_url_externallocation = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=pt_br&units=metric&APPID=${api_key}`;
+  callWeatherAPI(base_url_externallocation)
+})
 
